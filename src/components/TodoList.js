65544b9e55577f623c3a20bf0de0785ac85f1690
Zhen She <neal.seah@gmail.com>
2018-12-03 21:39:41 +0800

    feat: show input, add button and todos

---
+++ src/components/TodoList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <input/>
+        <button>add</button>
+        <div>todo1</div>
+        <div>todo2</div>
+        <div>todo3</div>
+      </div>
+    )
+  }
+}
