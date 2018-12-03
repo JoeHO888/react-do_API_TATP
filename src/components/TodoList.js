ec292249cdf0e73200e837318b69bc8fe11dff1b
Zhen She <neal.seah@gmail.com>
2018-12-03 21:55:55 +0800

    feat: can add new todo items

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react'
 import TodoInput from './TodoInput'
 
 export default class TodoList extends Component {
+  state = {todos: []}
+  addNewTodo = newTodo => {
+    this.state.todos.push(newTodo)
+    this.setState({todos: this.state.todos})
+  }
   render() {
     return (
       <div>
-        <TodoInput onNewTodoAdded={t => alert(t)}/>
-        <div>todo1</div>
-        <div>todo2</div>
-        <div>todo3</div>
+        <TodoInput onNewTodoAdded={this.addNewTodo}/>
+        {this.state.todos.map(todo => <div>{todo}</div>)}
       </div>
     )
   }
