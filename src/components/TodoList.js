fa6eaf42e2e44a3c8325036d05cc0cfb4a09a5de
Zhen She <neal.seah@gmail.com>
2018-12-05 15:52:36 +0800

    refactor: rename

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
-import TodoInput from '../containers/TodoInputContainer'
 import Todos from './Todos'
 import { connect } from 'react-redux'
+import TodoInputContainer from '../containers/TodoInputContainer';
 
 class TodoList extends Component {
   render() {
     return (
       <div>
-        <TodoInput/>
+        <TodoInputContainer/>
         <Todos todos={this.props.todos}/>
       </div>
     )
